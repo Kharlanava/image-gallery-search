3f048d3c5eda6b35c0aff81cdccb08e70b545536
Halina Kharlanava <galina.harlanova1@gmail.com>
2021-07-15 09:25:09 +0200

    First commit

---
+++ src/main/java/com/example/imagegallerysearch/ImageGallerySearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagegallerysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageGallerySearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageGallerySearchApplication.class, args);
+    }
+
+}
